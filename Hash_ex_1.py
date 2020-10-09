5562a0ad2cd16650b0b9d00cce8893e77e4f3a76
GaRam Song <21400389@handong.edu>
2020-10-09 23:27:18 +0900

    comments added

--- Hash_ex_1.py
+++ Hash_ex_1.py
@@ -1,26 +1,28 @@
+"""
+Date : 10/08/20
+Author : GaRam Song
+URL : https://programmers.co.kr/learn/courses/30/lessons/42576
+Description : hash algorithm
+"""
 # 선수의 수 <= 100,000
 # completion < participant (1)
 # 참가자 동명이인 있을 수 있음
 
-from collections import Counter
-
-def solution(participant, completion):
-    answer = ''
-    
-    return answer
-
 participant = ["mislav", "stanko", "mislav", "ana"]    # 참가자
 completion = ["stanko", "ana", "mislav"]   # 완주자
 
+# My Answer
+from collections import Counter
+
+def solution(participant, completion):
+    
+    return list((Counter(participant) - Counter(completion)).elements())[0]
 
 #print(list(filter(lambda x: x not in completion, participant)))
-res = map(lambda x: participant.remove(x), completion)
-print(list(res))
-
-print(list((Counter(participant) - Counter(completion)).elements()))
+#res = map(lambda x: participant.remove(x), completion)
 
 
-# hash를 사용한 풀이
+# Answer using Hash
 def solution(participant, completion):
     answer = ''
     temp = 0
--- Sort_ex_1.py
+++ Sort_ex_1.py
@@ -1,26 +1,38 @@
-#from operator import itemgetter
+"""
+Date : 10/08/20
+Author : GaRam Song
+URL : https://programmers.co.kr/learn/courses/30/lessons/42748
+Description : array sorting algorithm
+"""
+
+array = [1, 5, 2, 6, 3, 7, 4]
+commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
+
+# My Answer
+from operator import itemgetter
 
 def solution(array, commands):
-    #answer = []
+    answer = []
     
-    #for x in range(0, 3):
-    #    i = commands[x][0] # i
-    #    j = commands[x][1] # j
-    #    k = commands[x][2] # k
+    for x in range(0, 3):
+        i = commands[x][0] # i
+        j = commands[x][1] # j
+        k = commands[x][2] # k
 
         
-    #    new = sorted(array[i-1:j])    #i~j 까지의 배열
-    #    answer.append(new[k-1])
+        new = sorted(array[i-1:j])    #i~j 까지의 배열
+        answer.append(new[k-1])
         
         #new = sorted(array[commands[x][0]-1:commands[x][1]])
         
         #answer.append(new[commands[x][2]-1])
         
-    #return answer
+    return answer
+
+# Popular Answer
+def solution(array, commands):
 
     return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
 
-array = [1, 5, 2, 6, 3, 7, 4]
-commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 
 
