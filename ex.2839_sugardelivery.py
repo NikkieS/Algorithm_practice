80fc23418ab8652e1207006e6837910202d79ac8
GaRam Song <21400389@handong.edu>
2020-11-06 11:55:45 +0900

    daily algorithm practice 11/04

--- ex.2839_sugardelivery.py
+++ ex.2839_sugardelivery.py
@@ -9,14 +9,14 @@ order = int(input())
 bag = 0
 
 while True:
-    if order % 5 == 0 :
-        bag = bag + (order//5)
+    if order % 5 == 0 : #check if num is divisible by 5
+        bag = bag + (order//5)  # add divisor to the return value
         break
 
-    order = order -3
+    order = order -3    #if not divisible by 5 -> subtract 3 gradually
     bag += 1
     
-    if order < 0:
+    if order < 0:   #if not divisible by 5 or 3 -> return -1
         bag = -1
         break
 
